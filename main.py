30b2dace60e50f557e6dbab0446fa31c6dd04a53
Philip Whitehouse <philip@whiuk.com>
2014-04-26 15:54:28 +0100

    Further work on initial screen, MVC architecture

--- main.py
+++ main.py
@@ -2,26 +2,61 @@
 # main.py
 # Entry point for Application
 import wx
-class AgentFrame(wx.Frame):
-    def __init__(self, parent, title):
-         wx.Frame.__init__(self, parent, title=title, size=(640,480))
+from wx.lib.pubsub import Publisher as pub
+
+class DeviceData:
+    def __init__(self):
+         self.deviceName = "None"
+class MainView(wx.Frame):
+    def __init__(self, parent):
+         wx.Frame.__init__(self, parent, title='ANT Agent for Linux', size=(640,480))
+         # Menu Bar
          filemenu = wx.Menu()
-         aboutMenuItem = filemenu.Append(wx.ID_ABOUT, "&About", "Information about this program")
-         self.Bind(wx.EVT_MENU, self.OnAbout, aboutMenuItem)
+         self.aboutMenuItem = filemenu.Append(wx.ID_ABOUT, "&About", "Information about this program")
          filemenu.AppendSeparator()
-         exitMenuItem = filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
-         self.Bind(wx.EVT_MENU, self.OnExit, exitMenuItem)
+         self.exitMenuItem = filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
          menuBar = wx.MenuBar()
          menuBar.Append(filemenu, "&File")
          self.SetMenuBar(menuBar)
+         # Status Bar
+         statusBar = self.CreateStatusBar()
+         # Top Panel
+         topPanel = wx.Panel(self)
+         deviceData = wx.StaticText(topPanel, label="Device Details", pos=(20,0))
+         device = wx.StaticText(topPanel, label="Your device: ", pos=(40, 30))
+         status = wx.StaticText(topPanel, label="Status: ", pos=(40,60))
+         lastSync = wx.StaticText(topPanel, label="Last sync: ", pos=(40, 90))
+         self.scanForDevices = wx.Button(topPanel, wx.ID_ANY, "Scan for devices", pos=(40, 120))
+         # Bottom Panel
+         bottomPanel = wx.Panel(self)
+         activities = wx.StaticText(bottomPanel, label="Activities", pos=(20,0))
+         # Layout
+         self.sizer = wx.BoxSizer(wx.VERTICAL)
+         self.sizer.Add(topPanel, 1, wx.EXPAND)
+         self.sizer.Add(bottomPanel, 1, wx.EXPAND)
+         self.SetSizer(self.sizer)
+         self.SetAutoLayout(1)
+         # self.sizer.Fit(self)
          self.Show(True)
-    def OnAbout(self, event):
-	dlg = wx.MessageDialog( self, "A community-developed Linux version of the ANT Agent. Supports Garmin fitness devices", "About ANT Agent for Linux", wx.OK)
-        dlg.ShowModal()
-	dlg.Destroy()
+class MainController:
+    def __init__(self, app):
+        self.deviceData = DeviceData()
+        self.view = MainView(None)
+        self.view.scanForDevices.Bind(wx.EVT_BUTTON, self.ScanForDevices)
+        self.view.Bind(wx.EVT_MENU, self.OnAbout, self.view.aboutMenuItem)
+        self.view.Bind(wx.EVT_MENU, self.OnExit, self.view.exitMenuItem)
+        self.view.Show()
+    def ScanForDevices(self, evt):
+         """
+         TODO: Integrate device scanning here.
+         """
     def OnExit(self,e):
         self.Close(True)
+    def OnAbout(self, event):
+        dlg = wx.MessageDialog( self.view, "A community-developed Linux version of the ANT Agent. Supports Garmin-based fitness devices that communicate either over USB serial or via the ANT USB connector. Developed by Philip Whitehouse, based on work by Braiden Kindt and Gustav Tiger. Copyright 2014", "About ANT Agent for Linux", wx.OK);
+        dlg.ShowModal()
+        dlg.Destroy()
 
 app = wx.App(False)
-frame = AgentFrame(None, 'ANT Agent for Linux')
+controller = MainController(app)
 app.MainLoop()
